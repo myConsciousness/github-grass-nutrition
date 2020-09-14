6cc9819bbb61d4372ef96841735bcf904b004809
Kato Shinya <yourdream28@gmail.com>
2020-09-14 17:50:28 +0900

    update

--- src/js/grassNutrition.js
+++ src/js/grassNutrition.js
@@ -1,5 +1,21 @@
 "use strict";
 
+const LEGEND_COLOR = {
+  0: "ebedf0",
+  1: "9be9a8",
+  2: "40c463",
+  3: "30a14e",
+  4: "216e39",
+};
+
 window.onload = function () {
-  alert(document.getElementsByClassName("js-calendar-graph-svg"));
+  Array.from(document.getElementsByClassName("js-calendar-graph-svg")).forEach(
+    (svgWrapper) => {
+      Array.from(svgWrapper.getElementsByTagName("g"))
+        .slice(1)
+        .forEach((grass) => {
+          console.log(grass);
+        });
+    }
+  );
 };
