2231345b3ea32c5b2fcd46d2dc11fe062d4b7d6a
Kato Shinya <yourdream28@gmail.com>
2020-09-14 18:11:20 +0900

    update

--- src/js/grassNutrition.js
+++ src/js/grassNutrition.js
@@ -1,13 +1,5 @@
 "use strict";
 
-const LEGEND_COLOR = {
-  0: "ebedf0",
-  1: "9be9a8",
-  2: "40c463",
-  3: "30a14e",
-  4: "216e39",
-};
-
 window.onload = function () {
   Array.from(document.getElementsByClassName("js-calendar-graph-svg")).forEach(
     (svgWrapper) => {
