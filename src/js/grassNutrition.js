4664143e3265b499200031e062e61a6b96374f98
Kato Shinya <yourdream28@gmail.com>
2020-09-14 18:11:04 +0900

    update

--- src/js/grassNutrition.js
+++ src/js/grassNutrition.js
@@ -16,10 +16,29 @@ window.onload = function () {
         .forEach((grassGroup) => {
           Array.from(grassGroup.getElementsByTagName("rect")).forEach(
             (grass) => {
-              console.log(grass);
+              const contribution = getRandomContribution();
+              grass.setAttribute("data-count", contribution);
+              grass.setAttribute("fill", getLegendColor(contribution));
             }
           );
         });
     }
   );
+};
+
+const getRandomContribution = () => {
+  return Math.floor(Math.random() * 81);
+};
+
+const getLegendColor = (contribution) => {
+  if (contribution === 0 || contribution < 10) {
+    return "#ebedf0";
+  } else if (contribution < 20) {
+    return "#9be9a8";
+  } else if (contribution < 40) {
+    return "#40c463";
+  } else if (contribution < 60) {
+    return "#30a14e";
+  }
+  return "#216e39";
 };
